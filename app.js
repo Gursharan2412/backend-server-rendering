4369951246810857afeeec2caca71b60fdd41131
Gursharan Singh <gs7217013@gmail.com>
2024-12-13 21:13:58 +0100

    Added new features and files and modified app.js for improved functionality

--- app.js
+++ app.js
@@ -38,6 +38,17 @@ app.get('/like/:id', isLoggedIn, async function(req, res){
     res.redirect('/profile');
 });
 
+app.get('/edit/:id', isLoggedIn, async function(req, res){
+    let post = await postModel.findOne({_id: req.params.id}).populate('user');
+
+    res.render('edit', {post})
+});
+
+app.post('/update/:id', isLoggedIn, async function(req, res){
+    let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+    res.redirect('/profile');   
+});
+
 app.post('/post', isLoggedIn, async function(req, res){
     let user = await userModel.findOne({email: req.user.email});
     let {content} = req.body;
